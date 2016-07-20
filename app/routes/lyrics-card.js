a6d731c0844e4a96daacda0510414495cdc41690
Casey Watts <casey.s.watts@gmail.com>
2016-07-20 19:06:33 -0400

    display some sample data

--- app/routes/lyrics-card.js
+++ app/routes/lyrics-card.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return $.getJSON("data/mostCommonWords.json")
+  },
+  afterModel(model) {
+    model = model.slice(0,10);
+  }
 });
