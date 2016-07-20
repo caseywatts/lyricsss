2dc52d0ef89d43e1cb45b66e5aa83ab6237d936f
Casey Watts <casey.s.watts@gmail.com>
2016-07-20 19:17:01 -0400

    lyrics-card responds to click by choosing a new word

--- app/components/lyrics-card.js
+++ app/components/lyrics-card.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  aRandomLyric: 'Lyricsss',
+  someWords: ['a', 'b', 'c'],
+  click() {
+    let aRandomLyric = this.someWords.pop();
+    this.set('aRandomLyric', aRandomLyric)
+  }
 });
