ffc1ae6b8bcb9792f8f727324e5bb5862fb1be9c
Casey Watts <casey.s.watts@gmail.com>
2016-07-20 21:54:50 -0400

    use newest best set of words

--- app/routes/main.js
+++ app/routes/main.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.$.getJSON("data/5000MostCommonLyricWords.json");
+    return Ember.$.getJSON("data/3767MostCommonLyricWordsInDictionary.json");
   }
 });
