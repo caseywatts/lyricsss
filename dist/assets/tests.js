e53f54b060719c78d22011e4599ba8548e8c5e87
Casey Watts <casey.s.watts@gmail.com>
2020-09-01 23:05:54 -0400

    update to latest ember; install ember-jquery for now

--- dist/assets/tests.js
+++ dist/assets/tests.js
@@ -1,117 +1,56 @@
 'use strict';
 
-define('lyricsss/tests/app.lint-test', [], function () {
-  'use strict';
-
-  QUnit.module('ESLint | app');
-
-  QUnit.test('app.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'app.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('components/lyrics-card.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'components/lyrics-card.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('resolver.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'resolver.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('router.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'router.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('routes/main.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'routes/main.js should pass ESLint\n\n');
-  });
-});
-define('lyricsss/tests/integration/components/lyrics-card-test', ['ember-qunit'], function (_emberQunit) {
-  'use strict';
+define("lyricsss/tests/integration/components/lyrics-card-test", ["ember-qunit"], function (_emberQunit) {
+  "use strict";
 
   (0, _emberQunit.moduleForComponent)('lyrics-card', 'Integration | Component | lyrics card', {
     integration: true
   });
-
   (0, _emberQunit.test)('it renders', function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
+    this.render(Ember.HTMLBars.template(
+    /*
+      {{lyrics-card}}
+    */
+    {"id":"WI4gFMa1","block":"{\"symbols\":[],\"statements\":[[1,[34,0]]],\"hasEval\":false,\"upvars\":[\"lyrics-card\"]}","meta":{}}));
+    assert.equal(this.$().text().trim(), ''); // Template block usage:
 
-    this.render(Ember.HTMLBars.template({
-      "id": "0KH5e8SC",
-      "block": "{\"symbols\":[],\"statements\":[[1,[18,\"lyrics-card\"],false]],\"hasEval\":false}",
-      "meta": {}
-    }));
-
-    assert.equal(this.$().text().trim(), '');
-
-    // Template block usage:
-    this.render(Ember.HTMLBars.template({
-      "id": "O7BuS/8C",
-      "block": "{\"symbols\":[],\"statements\":[[0,\"\\n\"],[4,\"lyrics-card\",null,null,{\"statements\":[[0,\"      template block text\\n\"]],\"parameters\":[]},null],[0,\"  \"]],\"hasEval\":false}",
-      "meta": {}
-    }));
-
+    this.render(Ember.HTMLBars.template(
+    /*
+      
+        {{#lyrics-card}}
+          template block text
+        {{/lyrics-card}}
+      
+    */
+    {"id":"36/SYX0a","block":"{\"symbols\":[],\"statements\":[[2,\"\\n\"],[6,[37,0],null,null,[[\"default\"],[{\"statements\":[[2,\"      template block text\\n\"]],\"parameters\":[]}]]],[2,\"  \"]],\"hasEval\":false,\"upvars\":[\"lyrics-card\"]}","meta":{}}));
     assert.equal(this.$().text().trim(), 'template block text');
   });
 });
-define('lyricsss/tests/test-helper', ['lyricsss/app', 'lyricsss/config/environment', '@ember/test-helpers', 'ember-qunit'], function (_app, _environment, _testHelpers, _emberQunit) {
-  'use strict';
+define("lyricsss/tests/test-helper", ["lyricsss/app", "lyricsss/config/environment", "@ember/test-helpers", "ember-qunit"], function (_app, _environment, _testHelpers, _emberQunit) {
+  "use strict";
 
   (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
-
   (0, _emberQunit.start)();
 });
-define('lyricsss/tests/tests.lint-test', [], function () {
-  'use strict';
+define("lyricsss/tests/unit/routes/application-test", ["ember-qunit"], function (_emberQunit) {
+  "use strict";
 
-  QUnit.module('ESLint | tests');
-
-  QUnit.test('integration/components/lyrics-card-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'integration/components/lyrics-card-test.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('test-helper.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('unit/routes/application-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'unit/routes/application-test.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('unit/routes/lyrics-card-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'unit/routes/lyrics-card-test.js should pass ESLint\n\n');
-  });
-});
-define('lyricsss/tests/unit/routes/application-test', ['ember-qunit'], function (_emberQunit) {
-  'use strict';
-
-  (0, _emberQunit.moduleFor)('route:application', 'Unit | Route | application', {
-    // Specify the other units that are required for this test.
+  (0, _emberQunit.moduleFor)('route:application', 'Unit | Route | application', {// Specify the other units that are required for this test.
     // needs: ['controller:foo']
   });
-
   (0, _emberQunit.test)('it exists', function (assert) {
     let route = this.subject();
     assert.ok(route);
   });
 });
-define('lyricsss/tests/unit/routes/lyrics-card-test', ['ember-qunit'], function (_emberQunit) {
-  'use strict';
+define("lyricsss/tests/unit/routes/lyrics-card-test", ["ember-qunit"], function (_emberQunit) {
+  "use strict";
 
-  (0, _emberQunit.moduleFor)('route:lyrics-card', 'Unit | Route | lyrics card', {
-    // Specify the other units that are required for this test.
+  (0, _emberQunit.moduleFor)('route:lyrics-card', 'Unit | Route | lyrics card', {// Specify the other units that are required for this test.
     // needs: ['controller:foo']
   });
-
   (0, _emberQunit.test)('it exists', function (assert) {
     let route = this.subject();
     assert.ok(route);
@@ -122,7 +61,7 @@ define('lyricsss/config/environment', [], function() {
 try {
   var metaName = prefix + '/config/environment';
   var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
-  var config = JSON.parse(unescape(rawConfig));
+  var config = JSON.parse(decodeURIComponent(rawConfig));
 
   var exports = { 'default': config };
 
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'lyricsss/app';
+import config from 'lyricsss/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
